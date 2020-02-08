9707c13f1f706c245f82df7c49e55ae09ed24b20
Kuren <weryk153@gmail.com>
2020-02-08 18:36:46 +0800

    Gomoku 1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -38,6 +39,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html/'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 }
